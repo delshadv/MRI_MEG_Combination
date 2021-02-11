123ebdb5fcd450f2c78cd882ba7554ae407a2eb5
Delshad Vaghari <47316091+delshadv@users.noreply.github.com>
2021-02-11 20:57:06 +0000

    Update noise_sim.m
    Intermediate and late use the same hyper params as main results, Signal1 is now MMSE score

--- MKL/noise_sim.m
+++ MKL/noise_sim.m
@@ -21,19 +21,21 @@ mri_num      = grp2idx(participants.sImaging);
 mri_num([23 197]) = 2;
 participants(mri_num==2,:) = [];
 
-X = [participants.Edu_years participants.MMSE participants.age participants.sex]; 
+X = [participants.MMSE  participants.Edu_years participants.age participants.sex]; 
+%X = [participants.Edu_years  participants.MMSE participants.age participants.sex]; 
+X = zscore(X);
 
-y = csvread('derived/labels.csv'); % (better name for file?) What about labels?
+y = csvread('derived/labels.csv'); %
 
 No  = size(y,1) % Number of observations
 Nsf = size(X,2) % Number of signal features
-Nnf = 100       % Number of noise features
+Nnf = 1000       % Number of noise features
 
-Np = 100;      % Number of  noise randomisations (not permutations)
+Np = 1000;      % Number of  noise randomisations (not permutations)
 
 % test whether parpool  already open with numel(group etc...)
 if isempty(gcp('nocreate')) 
-    try parpool(min(Np,12)); end
+    try parpool(min(Np,100)); end
 end
 
 rng('default') % for reproducibility
@@ -53,12 +55,12 @@ parfor p = 1:Np % Number of noise realisations
     V{3} = {[Signal(:,1) Noise]}; % early combination (concatenation)
     % V{4} = {Signal(:,1), Noise};  % two separate kernels
     V{4} = mat2cell([Signal(:,1) Noise],No,ones(1,1+Nnf));  % all separate kernels
-    V{5} = {Signal(:,1), Signal(:,2:4)};
+    V{5} = mat2cell(Signal,No,ones(1,Nsf));
 
    % Intermediate better when C1=1 than C1=0.1; Late better when C1=0.1 
-   [acc1,~] = mkl_ens(V,y,'Hyper1',1,'CVratio',[0.8 0.2],'Nrun',1,'PCA_cut',0,'feat_norm',1,'ens',0);
-   [~,acc2] = mkl_ens(V,y,'Hyper1',0.1,'Hyper2',1,'CVratio',[0.8 0.2],'Nrun',1,'PCA_cut',0,'feat_norm',1,'ens',1);
-%    [acc1,acc2] = mkl_ens(V,y,'Hyper1',0.1,'Hyper2',1,'CVratio',[0.8 0.2],'Nrun',1,'PCA_cut',0,'feat_norm',1,'ens',1);
+%   [acc1,~] = mkl_ens(V,y,'Hyper1',1,'CVratio',[0.8 0.2],'Nrun',1,'PCA_cut',0,'feat_norm',1,'ens',0);
+%  [~,acc2] = mkl_ens(V,y,'Hyper1',0.1,'Hyper2',1,'CVratio',[0.8 0.2],'Nrun',1,'PCA_cut',0,'feat_norm',1,'ens',1);
+    [acc1,acc2] = mkl_ens(V,y,'Hyper1',0.1,'Hyper2',1,'CVratio',[0.8 0.2],'Nrun',1,'PCA_cut',0,'feat_norm',1,'ens',1);
     
     accuracy1{p} = mean(mean(acc1,3),1);
     accuracy2{p} = mean(mean(acc2,3),1);
@@ -70,6 +72,8 @@ mean(accuracy1)
 accuracy2 = cat(1,accuracy2{:});
 mean(accuracy2)
 
+save noise_sim accuracy1 accuracy2
+
 % Plot resluts (Classification accuracy and Pos-hoc comparison)
 
 titles = {'Noise','Signal1','Signal1+Noise','Signal1,Noise','Signal1,Signal2-4'};
@@ -77,17 +81,19 @@ titles = {'Noise','Signal1','Signal1+Noise','Signal1,Noise','Signal1,Signal2-4'}
 c   = [-1 1 0 0 0;
        0 1 0 -1 0;
        0 0 -1 1 0];
-con_titles = {'Signal1>Noise','Signal1>Signal1,Noise','Signal1,Noise>Signal1+Noise'};
+%con_titles = {'Signal1>Noise','Signal1>Signal1,Noise','Signal1,Noise>Signal1+Noise'};
 
-% [f1] = plot_results(titles,accuracy1,con_titles,c)
-% sgtitle(sprintf('Intermediate, Nf = %d', Nnf))
+ [f1] = plot_results(titles,accuracy1)%,con_titles,c)
+ sgtitle(sprintf('Intermediate, Nf = %d', Nnf))
 % 
-% [f1] = plot_results(titles,accuracy2,con_titles,c)
-% sgtitle(sprintf('Late, Nf = %d', Nnf))
+[f2] = plot_results(titles,accuracy2,con_titles,c)
+sgtitle(sprintf('Late, Nf = %d', Nnf))
 
 [f1] = plot_results(cat(2,titles,titles),[accuracy1 accuracy2],cat(2,titles,titles),[-eye(5) eye(5)])
 sgtitle(sprintf('Intermediate+Late, Nf = %d', Nnf))
-
+%[f1] = plot_results(cat(2,titles,titles),[accuracy1 accuracy2],cat(2,titles,titles),[-eye(5) eye(5)])
+[f3] = plot_results(titles,accuracy1,titles,[-eye(5) eye(5)],[accuracy1 accuracy2]),
+sgtitle(sprintf('Late - Intermediate'))
 
 return
 
